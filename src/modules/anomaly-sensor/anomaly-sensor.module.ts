7efb76fea277d6564d146bffeff5529218f007d9
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-07-05 13:01:00 +0300

    chore: scaffold AnomalySensorModule structure

---
+++ src/modules/anomaly-sensor/anomaly-sensor.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * AnomalySensorModule - Initial module scaffold for anomaly detection.
+ * This module will house all anomaly detection and alerting services.
+ */
+@Module({})
+export class AnomalySensorModule {}
